ea7a8aef4f9386cc32e8c51f5d338169febe1dcd
venkatesh <venkateshr590@gmail.com>
2021-01-31 19:44:59 +0530

    data

--- src/Components/MainProduct.js
+++ src/Components/MainProduct.js
@@ -1,19 +1,6 @@
-import React, { Component } from 'react'
-import iphone from '../Assets/iphone.jpg';
-import bads from '../Assets/beds.jpg';
-import tshrit from '../Assets/t-shrit.jpg';
-import BrandParent from './BrandParent';
-import Filter from './Filter';
-import Products from './Products';
-import stars from '../Assets/stars.png';
-import { Link } from 'react-router-dom';
-import MainProduct from './MainProduct';
+import React, { Component } from 'react';
 
-
-
-
-
-export class Categories extends Component {
+export class Products extends Component {
     constructor(props){
         super(props);
         this.state={
@@ -74,19 +61,11 @@ export class Categories extends Component {
                 },
             
             ],
-          
             filteredProducts:[],
-
         }
         this.handleChangeSort = this.handleChangeSort.bind(this)
+
     }
-    // componentDidMount() {
-    //     fetch('http://localhost:8000/products').then(res => res.json())
-    //     .then(data => this.setState({
-    //         products:data,
-    //         filteredProducts:data
-    //     }))
-    // }
     handleChangeSort (e) {
         this.setState({sort: e.target.value});
         this.listProducts();
@@ -95,64 +74,33 @@ export class Categories extends Component {
         this.setState(state => {
             if(state.sort !== ''){
                 state.products.sort((a,b)=>(state.sort === 'lowest')? 
-                (a.price > b.price?1:-1): 
-                (a.price < b.price?1:-1))
+                (a.price < b.price?1:-1): 
+                (a.price > b.price?1:-1))
             } else {
                 state.products.sort((a,b)=>(a.id<b.id?1:-1));
             }return {filteredProducts: state.products};
         })
     }
-
+    
     render() {
-        return (
-            <div className="gridBox">
-                <div className="sideBar">
-                    <div className="content">
-                        <h6>Categories</h6>
-                        <div className="link">
-                            <ul className="lists">
-                                <li><Link to='/accessories' className=" ">Home</Link></li>
-                                <li><Link to='/technologies' className=" ">Technologies</Link></li>
-                                <li><Link to='/humans' className=" ">Men / Women</Link></li>
-                                <li><Link to='/edu' className=" ">Educations</Link></li>
-                            </ul>
+        const productItem = this.props.products.map( product => (
+            <div className="cards m-1" key={product.id}>
+                <div className="card" style={{width: "15rem"}}>
+                    <img src={product.images} style={{objectFit:"cover", height:"30vh"}}  className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h6 className="card-title">{product.title}</h6>
+                            <p className="card-text">{product.price}</p>
                         </div>
-                    <div className="filter">
-                        <p>Filter by price</p>
-                        <div className="inner-filter">
-                            <button className="btn btn-filter">Filter</button>
-                            <p>Price $0 - $899</p>
-                        </div>
-                    </div>
-                    <div className="filter">
-                        <p>Top products</p>
-                            <BrandParent brandImages={iphone} brandtitle="iPhone 11" brandPrice="$599"  stars={stars}/>
-                            <BrandParent brandImages={bads} brandtitle="Solimo Optima" brandPrice="$599" stars={stars}/>
-                            <BrandParent brandImages={tshrit} brandtitle="Plain Polo" brandPrice="$599" stars={stars}/>
-                        </div>
-                    </div> 
-                    
-                    <div className="itemsContent">
-                        <div className="filterBox">
-                        <Filter 
-                            size={this.state.size}
-                            sort={this.state.sort}
-                            handleChangeSize={this.handleChangeSize}
-                            handleChangeSort={this.handleChangeSort}
-                            count={this.state.filteredProducts.length}
-                        /><br/>
-                    </div>
-                    <div className="mainProductList">
-                        <Products
-                            products={this.state.filteredProducts}
-                            handleAddToCart={this.state.handleAddToCart}
-                        />
-                    </div>
-                    </div>
                 </div>
+            </div>
+            )
+        )
+        return (
+            <div className="cards" >
+                {productItem}
             </div>
         )
     }
 }
 
-export default Categories
+export default Products
